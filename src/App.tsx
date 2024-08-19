3453deac17f2875323da259f89c8a4f965181eca
j-sauceda <jorge.sauceda@gmail.com>
2024-08-19 17:42:53 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import QRCodeScanner from "./components/QRCodeScanner";
+
+import "./App.css";
+
+function App() {
+  const [isScanningPaused, setIsScanningPaused] = useState(false);
+  const [scannedData, setScannedData] = useState<string | null>(null);
+
+  const handleScan = (data: string) => {
+    setScannedData(data);
+    console.log(data);
+  };
+
+  function isValidUrl(url: string): boolean {
+    const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})\/([\w.-]*)*\/?$/;
+    return urlRegex.test(url);
+  }
+
+  return (
+    <div className="container">
+      <div className="header">Simple QR Scanner</div>
+      <div className="content">
+        {scannedData ? (
+          <div style={{ margin: "10%", overflowWrap: "break-word" }}>
+            <p>
+              <b>Result</b>:
+            </p>
+            {isValidUrl(JSON.parse(scannedData)?.rawValue) ? (
+              <a href={JSON.parse(scannedData)?.rawValue} target="_blank">
+                {JSON.parse(scannedData)?.rawValue}
+              </a>
+            ) : (
+              JSON.parse(scannedData)?.rawValue
+            )}
+          </div>
+        ) : (
+          <QRCodeScanner
+            isPaused={isScanningPaused}
+            onScan={handleScan}
+            setIsPaused={setIsScanningPaused}
+          />
+        )}
+        {isScanningPaused && (
+          <button
+            onClick={() => {
+              setScannedData(null);
+              setIsScanningPaused(false);
+            }}
+          >
+            Keep Scanning
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QRCodeScanner.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
+
+import "./Scanner.css";
+
+interface QRCodeScannerProps {
+  isPaused: boolean;
+  onScan: (data: string) => void;
+  setIsPaused: (isPaused: boolean) => void;
+}
+
+const QRCodeScanner: FC<QRCodeScannerProps> = ({
+  isPaused,
+  onScan,
+  setIsPaused,
+}) => {
+  const handleScan = (data: IDetectedBarcode[]) => {
+    if (!!data) {
+      let result = "";
+      data.forEach((datum) => {
+        result = result + JSON.stringify(datum);
+      });
+      onScan(result);
+    }
+    setIsPaused(true);
+  };
+
+  const handleError = (error: any) => {
+    console.error(error);
+  };
+
+  return (
+    <Scanner
+      allowMultiple={false}
+      classNames={{ container: "scanner" }}
+      components={{ audio: false, finder: false }}
+      constraints={{ aspectRatio: 1.0, facingMode: "environment" }}
+      onError={handleError}
+      onScan={(res) => handleScan(res)}
+      paused={isPaused}
+      scanDelay={500}
+    />
+  );
+};
+
+export default QRCodeScanner;
